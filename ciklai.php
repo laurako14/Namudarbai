7677a0fd56d4ac56f9b6560f3ffe29918440ec62
LauraKo <kovarskyte@yahoo.com>
2021-02-11 21:21:44 +0200

    Stringai ir dalis ciklu

---
+++ ciklai.php
@@ -0,0 +1,221 @@
+<!-- <?php 
+echo "1. --------------", "<br>";
+$linija = str_repeat("*", 400);
+echo "<p style='word-wrap: break-word;'>". $linija. "</p>";
+for ($i = 0; $i < (400/50); $i++) {
+    echo "<p style='word-wrap: break-word;'>". str_repeat("*", 50). "</p>";
+}
+echo '<br><br>';
+?>
+
+<?php 
+echo "2. --------------", "<br>";
+$did150 = 0;
+for ($i = 0; $i < 300; $i++) {
+    $genSk = rand(0, 300). " ";
+    if ($genSk > 150) {
+        $did150++;
+    }
+    if ($genSk > 275) {
+        echo "<span style=color:red;> $genSk </span>";
+    } else {
+        echo "<span> $genSk </span>";
+    }
+}
+
+echo '<br><br>';
+?>
+
+<?php 
+echo "3. --------------", "<br>";
+$random = rand(3000, 4000);
+$ats = "77";
+for ($i = $ats+1; $i < $random; $i++) {
+    if ($i % 77 == 0) {
+        $ats .= ","."<span> $i </span>";
+    }
+}
+echo $ats;
+echo '<br><br>';
+?>
+
+<?php 
+echo "4. --------------", "<br>";
+for ($i = 0; $i < 25; $i++) {
+    for ($j = 0; $j < 25; $j++) {
+        echo "<span style='padding-right: 10px;'> * </span>";
+    }
+    echo "<br>";
+}
+echo '<br><br>';
+?>
+
+<?php 
+echo "5. --------------", "<br>";
+for ($i = 0; $i < 25; $i++) {
+    for ($j = 0; $j < 25; $j++) {
+        if ($j == $i || $j == (24 - $i)) {
+            echo "<span style='padding-right: 10px; color:red;'> * </span>";
+        } else {
+            echo "<span style='padding-right: 10px;'> * </span>";
+        }
+    }
+    echo "<br>";
+}
+echo '<br><br>';
+?> -->
+
+<?php 
+echo "6. --------------", "<br>";
+$rez = "";
+do {
+    $moneta = rand(0, 1);
+    if ($moneta == 0) {
+        $rez .= "H";
+        echo "H", "<br>";
+    } else {
+        $rez .= "S";
+        echo "S", "<br>";
+    }
+} while (!str_contains($rez, "H"));
+echo '<br><br>';
+$rez = "";
+do {
+    $moneta = rand(0, 1);
+    if ($moneta == 0) {
+        $rez .= "H";
+        echo "H", "<br>";
+    } else {
+        $rez .= "S";
+        echo "S", "<br>";
+    }
+} while (!str_contains($rez, "HHH"));
+echo '<br><br>';
+$count = 0;
+do {
+    $moneta = rand(0, 1);
+    if ($moneta == 0) {
+        $count++;
+        echo "H", "<br>";
+    } else {
+        echo "S", "<br>";
+    }
+} while ($count != 3);
+echo '<br><br>';
+?>
+
+<?php 
+echo "7. --------------", "<br>";
+$player1 = 'Petras';
+$player2 = 'Kazys';
+$play = true;
+$score1 = 0;
+$score2 = 0;
+while ($play) {
+    if ($score1 >= 222) {
+        echo "Partija laimejo: $player1";
+        $play = false;
+    } elseif ($score2 >= 222) {
+        echo "Partija laimejo: $player2";
+        $play = false;
+    } else {
+    $petrasTaskai = rand(10, 20);
+    $score1 += $petrasTaskai;
+    $kazysTaskai = rand(5, 25);
+    $score2 += $kazysTaskai;
+    }
+}
+echo '<br><br>';
+?>
+
+<?php 
+echo "8. --------------", "<br>";
+$x = 1;
+echo "<div style='text-align:center;'>";
+for ($i = 0; $i < 10; $i++) {
+        for ($j = 0; $j < $x; $j++) {
+            echo "<span style='padding-right: 10px; color: rgb(".rand(0, 255).",". rand(0, 255).",". rand(0, 255).");'> * </span>";
+        }
+        $x++;
+        echo "<br>";
+}
+$x--;
+for ($i = 10; $i >= 0; $i--) {
+    for ($j = $x; $j >= 0; $j--) {
+        echo "<span style='padding-right: 10px; color: rgb(".rand(0, 255).",". rand(0, 255).",". rand(0, 255).");'> * </span>";
+    }
+    $x--;
+    echo "<br>";
+}
+echo "</div>";
+echo '<br><br>';
+?>
+
+<?php 
+echo "9. --------------", "<br>";
+$c = "10 bezdzioniu \n suvalge 20 bananu.";
+$s = '10 bezdzioniu \n suvalge 20 bananu.';
+$a = microtime();
+for ($i = 0; $i < 1000000; $i++) {
+    $c;
+}
+$b = microtime();
+echo (float) $duration = $b - $a;
+$d = microtime();
+for ($i = 0; $i < 1000000; $i++) {
+    $s;
+}
+$e = microtime();
+echo (float) $duration = $e - $d;
+echo '<br><br>';
+?>
+
+<?php 
+echo "10. --------------", "<br>";
+$count = 0;
+$finish = 0;
+do {
+    $smugis = rand(5, 20);
+    $finish += $smugis;
+    $count++;
+} while ($finish < 85*5);
+echo "5 vinys ikalti $count mazais smugiais";
+echo '<br><br>';
+$count = 0;
+$finish = 0;
+do {
+    $smugis = rand(20, 30);
+    $tikimybe = rand(0,1);
+    if ($tikimybe == 1) {
+    $finish += $smugis;
+    $count++;
+    }
+} while ($finish < 85*5);
+echo "5 vinys ikalti $count dideliais smugiais";
+echo '<br><br>';
+?>
+
+<?php 
+echo "11. --------------", "<br>";
+$string = "";
+for ($i = 0; $i < 150; $i++) {
+    $newNumber = rand(1, 200);
+    if (str_contains($string, $newNumber)) {
+        continue;
+    } else {
+        $string .= $newNumber." ";
+    }
+    if (count(explode(" ", $string)) == 51) {
+        break;
+    }
+}
+echo $string;
+$string2 = "";
+for ($i = 0; $i < strlen($string); $i++) {
+    if ((int) $string[$i] % (int) $string[$i] == 0) {
+        $string2 .= $string[$i]." ";
+    }
+}
+echo $string2;
+echo '<br><br>';
+?>
--- stringai.php
+++ stringai.php
@@ -43,7 +43,7 @@ echo '<br><br>';
 <?php
 echo "5. --------------", "<br>";
 $string = "An American in Paris";
-$find = array("a", "A");
+$find = ["a", "A"];
 echo str_replace($find, "*", $string);
 echo '<br><br>';
 ?>
@@ -61,7 +61,7 @@ $string = "An American in Paris";
 $string2 = "Breakfast at Tiffany's";
 $string3 = "2001: A Space Odyssey";
 $string4 = "It's a Wonderful Life";
-$find = array("a", "A", "e", "E", "i", "I", "o", "O");
+$find = ["a", "A", "e", "E", "i", "I", "o", "O"];
 echo str_replace($find, "", $string), "<br>";
 echo str_replace($find, "", $string2), "<br>";
 echo str_replace($find, "", $string3), "<br>";
@@ -71,21 +71,66 @@ echo '<br><br>';
 
 <?php 
 echo "8. --------------", "<br>";
-$string = 'Star Wars: Episode'.str_repeat(' ', rand(0,5)). rand(1,9).'A New Hope';
-echo filter_var($string, FILTER_SANITIZE_NUMBER_INT);
+$string = 'Star Wars: Episode'.str_repeat(' ', rand(0,5)). rand(1,9).' - A New Hope';
+echo (int) filter_var($string, FILTER_SANITIZE_NUMBER_INT);
 echo '<br><br>';
 ?>
 
 <?php 
 echo "9. --------------", "<br>";
-$string = "Don't Be a Menace to Sount Central While Drinking Your Juice in the Hood";
+$string = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood";
+$string2 = "Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale";
 $words = explode(" ", $string);
+$words2 = explode(" ", $string2);
 $count = 0;
 for ($i = 0; $i < count($words); $i++) {
-    if (count($words[$i]) <= 5) {
+    if (mb_strlen($words[$i]) <= 5) {
         $count++;
     }
 }
-echo $count, "<br>";
+$count2 = 0;
+for ($i = 0; $i < count($words2); $i++) {
+    if (mb_strlen($words2[$i]) <= 5) {
+        $count2++;
+    }
+}
+echo $count, "<br>", $count2, "<br>";
+echo '<br><br>';
+?>
+
+<?php 
+echo "10. --------------", "<br>";
+$a_z = "abcdefghijklmnoprstuvzABCDEFGHIJKLMNOPRSTUVZ";
+$new = "";
+for ($i = 0; $i < 3; $i++) {
+    $int = rand(0,44);
+    $new .= $a_z[$int];
+}
+echo $new, "<br>";
+
+echo '<br><br>';
+?>
+
+<?php 
+echo "11. --------------", "<br>";
+$string = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood";
+$string2 = "Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale";
+$new = "";
+$words = explode(" ", $string);
+$words2 = explode(" ", $string2);
+$wordsArray = array_unique(array_merge($words, $words2));
+for ($i = 0; $i < 100; $i++) {
+    $int = rand(0, count($wordsArray) - 1);
+    $newWord = $wordsArray[$int];
+    if (str_contains($new, $newWord)) {
+       continue;
+    } else {
+        $new .= $newWord." ";
+    }
+    if (count(explode(" ", $new)) == 11) {
+        break;
+    }
+}
+echo $new, "<br>";
 echo '<br><br>';
 ?>
